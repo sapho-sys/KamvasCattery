744cd64969d856a20a0feb3783a7e5c0e2f828e2
sapho-sys <saphonkunzi@gmail.com>
2022-04-22 16:04:00 +0200

    complete!

--- bookings_cost/test/bookings.tests.js
+++ bookings_cost/test/bookings.tests.js
@@ -1,1 +1,40 @@
+describe("this test",function(){
+    it("should return R135.00 for MONDAY check-ins",function(){
+        var booking={
+            check_in_day: 'Monday',
+            duration: 3,
+            cat_name : 'Snowy'
+          }
+          
+          
+        assert.equal(135.00, bookingCost(booking));
 
+
+
+    })
+    it("should return R101.25 for WEDNESDAY check-ins after discount calculations",function(){
+        var booking={
+            check_in_day: 'Wednesday',
+            duration: 3,
+            cat_name : 'Snowy'
+          }
+        assert.equal(101.25, bookingCost(booking));
+
+
+        
+    })
+    it("should return R114.75 for FRIDAY check-ins after discount calculations",function(){
+        var booking={
+            check_in_day: 'Friday',
+            duration: 3,
+            cat_name : 'Snowy'
+          }
+          
+          
+        
+        assert.equal(114.75, bookingCost(booking));
+
+
+        
+    })
+})
--- weekly_wages/weekly_wages.js
+++ weekly_wages/weekly_wages.js
@@ -1,3 +1,31 @@
-function weeklyWage(band, weeklyHours) {
+function weeklyWage(band, weeklyHours){
+   var totalPay=0;
+    var total=0;
+    var hours=weeklyHours.split(",");
+    var count=0;
+    
+  let lessHours = hours.length
+   
+  if(lessHours < 7) {
+    return "missing day"
+  }
+  
+    for(var i = 0; i < hours.length; i++) { 
+      if(band==="A"){
+          total+= Number(hours[i]);
+      totalPay = total * 23;
 
-};
+      }else if(band==="B"){
+        total += Number(hours[i]);
+        totalPay = total * 37;
+        
+  } else if(band==="C"){
+    total += Number(hours[i]);
+    totalPay = total * 51;
+    
+  } 
+    }
+    return totalPay;
+    
+    
+  }
